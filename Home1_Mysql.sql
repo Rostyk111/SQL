d570535fe4607190a3fb2ab4c4ef7806d6aecd2d
Rostyk111 <61513213+Rostyk111@users.noreply.github.com>
2020-05-29 20:37:20 +0300

    Add files via upload

---
+++ Home1_Mysql.sql
@@ -0,0 +1,14 @@
+SELECT * FROM library.book;
+
+INSERT INTO book (name, autor, categoty)
+VALUES
+("Anna Karenina","Leo Tolstoy","romane"),
+("War and Peace","Leo Tolstoy","romane"),
+("The Adventures of Huckleberry Finn ","Mark Twain","adventures"),
+("Hamlet","William Shakespeare","romane"),
+("Great Expectations"," Charles Dickens","artistic autobiography"),
+("Crime and Punishment ","Fyodor Dostoyevsky","romane"), 
+("The Odyssey","Homer","epopee"), 
+("The Master and Margarita","Mikhail Bulgakov","romane"), 
+("Don Quixote ","Miguel de Cervantes","adventures"),
+("The Adventures of Tom Sawyer","Mark Twain","adventures")
